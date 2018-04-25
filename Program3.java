a730392f226c304ca0de13e7d36d43705df11a67
Aria Pahlavan <apahlavan1@utexas.edu>
2018-04-24 19:55:19 -0500

    Implement iterative and recursive solution to part 2 to compute maximum loss

--- Program3.java
+++ Program3.java
@@ -3,20 +3,13 @@
 //EID: AP44342
 
 
-import java.util.function.BiFunction;
-
 public class Program3 {
     EconomyCalculator calculator;
     VibraniumOreScenario vibraniumScenario;
 
-    BiFunction<Integer, Integer, Integer> f = (n, v) -> calculator.calculateGain(n, v);
-
-    private int[][] memo;
-
     public Program3() {
         this.calculator = null;
         this.vibraniumScenario = null;
-        this.memo = null;
     }
 
     /**
@@ -26,7 +19,6 @@ public class Program3 {
      */
     public void initialize(EconomyCalculator ec) {
         this.calculator = ec;
-        this.memo = new int[ec.getNumProjects()][ec.getNumVibranium()+1];
     }
 
     /**
@@ -38,6 +30,10 @@ public class Program3 {
         this.vibraniumScenario = vs;
     }
 
+    private int f(int n, int v) {
+        return calculator.calculateGain(n, v);
+    }
+
     /**
      * This method returns an integer that is maximum possible gain in the Wakandan economy
      * given a certain amount of Vibranium
@@ -45,6 +41,7 @@ public class Program3 {
     public int computeGain() {
         int N = calculator.getNumProjects();
         int V = calculator.getNumVibranium();
+        int[][] memo = new int[N][V + 1];
 
         for (int i = 0; i < N; i++)
             for (int j = 0; j <= V; j++)
@@ -53,12 +50,13 @@ public class Program3 {
         for (int i = 0; i <= V; i++)
             memo[0][i] = calculator.calculateGain(0, i);
 
+        return opt1Rec(N - 1, V, memo);
 /*
         for (int i = 1; i < N; i++) {
             int max = 0;
 
             for (int j = 1; j <= V; j++) {
-                memo[i][j] = Math.max(memo[i-1][j], f.apply(i, j));
+                memo[i][j] = Math.max(memo[i-1][j], f(i, j));
 
                 if (memo[i][j] > max)
                     max = memo[i][j];
@@ -70,48 +68,135 @@ public class Program3 {
 
         return res[N-1];
 */
-        int maxGrowth = of(N-1, V, memo, N, V);
-
-//        Arrays.stream(memo).forEach(vs -> {
-//            Arrays.stream(vs).forEach(v -> System.out.print(v + "\t"));
-//            System.out.println();
-//        });
-
-        return maxGrowth;
     }
 
-    private int of(int n, int v, int[][] memo, int N, int V) {
-        if (n < 0 || n >= N || v < 1 || v > V) return 0;
-        if (memo[n][v] != Integer.MIN_VALUE) return memo[n][v];
-        if (v == 1) return Math.max(f.apply(n, 1), memo[n-1][1]);
+    /**
+     * Iterative solution to Compute Gain problem
+     *
+     * @param N number of projects
+     * @param V amount Vibranium available
+     * @param memo memoization table to be used
+     * @return max economical gain
+     */
+    private int opt1Iter(int N, int V, int[][] memo) {
+
+        for (int n = 1; n < N; n++) {
+            for (int v = 0; v <= V; v++) {
+
+            }
+        }
+
+        return 0;
+    }
+
+
+    /**
+     * Recursive solution to Compute Gain problem
+     *
+     * @param N number of projects
+     * @param V amount Vibranium available
+     * @param memo memoization table to be used
+     * @return max economical gain
+     */
+    private int opt1Rec(int N, int V, int[][] memo) {
+        if (N < 0 || V < 1) return 0;
+        if (memo[N][V] != Integer.MIN_VALUE) return memo[N][V];
+//        if (V == 1) return Math.max(f(N, 1), memo[N - 1][1]);
 
         int max = Integer.MIN_VALUE;
 
-        for (int j = 0; j <= v; j++) {
-            if (memo[n-1][j] == Integer.MIN_VALUE)
-                memo[n-1][j] = of(n-1, j, memo, N, V);
+        for (int j = 0; j <= V; j++) {
+            if (memo[N - 1][j] == Integer.MIN_VALUE)
+                memo[N - 1][j] = opt1Rec(N - 1, j, memo);
 
-            max = Math.max(
-                    max,
-                    memo[n-1][j] + f.apply(n, v-j)
+            max = Math.max(max, memo[N - 1][j] + f(N, V - j)
             );
         }
 
-        memo[n][v] = max;
+        memo[N][V] = max;
 
         return max;
     }
 
     /**
-     * This method returns an integer that is the maximum possible dollar value that a thief 
-     * could steal given the weight and volume capacity of his/her bag by using the 
+     * This method returns an integer that is the maximum possible dollar value that a thief
+     * could steal given the weight and volume capacity of his/her bag by using the
      * VibraniumOreScenario instance.
      */
-     public int computeLoss() {
-         //TODO: Complete this method
+    public int computeLoss() {
+        int N = vibraniumScenario.getNumOres();
+        int W = vibraniumScenario.getWeightCapacity();
+        int V = vibraniumScenario.getVolumeCapacity();
 
-        return 0;
-     }
+        return opt2Iter(N, W, V);
+    }
+
+    //
+    //             0                                                       if i == 0
+    // OPT(i,w) =  OPT(i−1, W, V)                                          if wi > W or vi > V
+    //             max(OPT(i−1, W, V), pi + OPT(i−1, W−wi, V-vi))          otherwise
+    //
+
+    /**
+     * Iterative solution to Compute Loss problem
+     *
+     * @param n number of items to steal
+     * @param W Weight capacity of knapsack
+     * @param V Volume capacity of knapsack
+     * @return max loss
+     */
+    private int opt2Iter(int n, int W, int V) {
+        int[][][] memo2 = new int[n + 1][W + 1][V + 1];
+
+        for (int w = 1; w <= W; w++)
+            for (int v = 1; v <= V; v++)
+                memo2[0][w][v] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            VibraniumOre item = vibraniumScenario.getVibraniumOre(i - 1);
+            int pi = item.getPrice();
+            int wi = item.getWeight();
+            int vi = item.getVolume();
+
+            for (int w = 1; w <= W; w++)
+                for (int v = 1; v <= V; v++)
+                    if (wi > w || vi > v) memo2[i][w][v] = memo2[i - 1][w][v];
+                    else memo2[i][w][v] = Math.max(memo2[i - 1][w][v],
+                                                   memo2[i - 1][w - wi][v - vi] + pi);
+        }
+
+        return memo2[n][W][V];
+    }
+
+    /**
+     * Recursive solution to Compute Loss problem
+     *
+     * @param i     number of items to steal
+     * @param W     Weight capacity of knapsack
+     * @param V     Volume capacity of knapsack
+     * @param memo2 memoization table to be used
+     * @return max loss
+     */
+    private int opt2Rec(int i, int W, int V, int[][][] memo2) {
+        if (i == 0) return 0;
+
+        if (memo2[i][W][V] != 0) return memo2[i][W][V];
+
+        VibraniumOre item = vibraniumScenario.getVibraniumOre(i - 1);
+        int pi = item.getPrice();
+        int wi = item.getWeight();
+        int vi = item.getVolume();
+
+        if (wi > W || vi > V) {
+            memo2[i][W][V] = opt2Rec(i - 1, W, V, memo2);
+            return memo2[i][W][V];
+        } else {
+            memo2[i][W][V] = Math.max(opt2Rec(i - 1, W, V, memo2),
+                                      opt2Rec(i - 1, W - wi, V - vi, memo2) + pi);
+            return memo2[i][W][V];
+        }
+    }
+
 }
 
 
